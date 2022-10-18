7ecd6e8d6cfeafe04d8b5544eb07053f57715218
Marina Mordovkina <m_mordovkina@mail.ru>
2022-10-18 17:03:39 +0300

    Программа для нахождения большего из двух чисел

---
+++ Seminar1_DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введит число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введит число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+{
+    Console.WriteLine("Числа равны, введита два разных числа");
+}
+else
+{
+if (number1 > number2) Console.WriteLine($"Число {number1} больше числа {number2}");
+if (number2 > number1) Console.WriteLine($"Число {number2} больше числа {number1}");
+}
