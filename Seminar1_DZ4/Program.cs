70a176414ebce9fa88165405b14c693c22f79d5a
Marina Mordovkina <m_mordovkina@mail.ru>
2022-10-18 20:22:04 +0300

    Программа определения четных чисел от 1 до N

---
+++ Seminar1_DZ4/Program.cs
@@ -0,0 +1,11 @@
+﻿   // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+    Console.WriteLine("Введите число: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ int number = 1;
+ Console.Write($"Четные числа от 1 до {n}: ");
+        while (number <= n)
+        {
+            if (number % 2 == 0) Console.Write($"{number} ");
+            number = number + 1;
+        }
+        
