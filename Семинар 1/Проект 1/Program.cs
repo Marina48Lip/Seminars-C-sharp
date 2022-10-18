ce9e42d8fbdb80fda4cc0881812033286be22dce
Marina Mordovkina <m_mordovkina@mail.ru>
2022-10-18 15:49:58 +0300

    Программа квадрат числа

---
+++ Семинар 1/Проект 1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход целое число и выдает его квадрат
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrNumber = number * number;
+Console.WriteLine($"Квадрат вашего числа = {sqrNumber}");
+
