61a0c2d60af9fca7be9dedf5b4a7d98674f10c3a
Marina Mordovkina <m_mordovkina@mail.ru>
2022-10-26 12:45:06 +0300

    Программа вывода таблицы кубов чисел от 1 до N

---
+++ Seminar3_DZ3/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void printCubeToConsole(int n)
+{
+    for (int count = 1; count <= n; count++)
+    {
+               Console.WriteLine(count*count*count);
+    }
+}
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+printCubeToConsole(n);
