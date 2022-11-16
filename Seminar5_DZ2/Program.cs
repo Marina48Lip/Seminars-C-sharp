8e6f3d1577316dce1bd0c58b53154af33e61ee94
Marina Mordovkina <m_mordovkina@mail.ru>
2022-11-16 12:06:13 +0300

    Программа подсчета суммы элементов массива, стоящих на нечетных позициях

---
+++ Seminar5_DZ2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+int[] array = new int[12];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 25);
+    Console.Write(array[i] + " ");
+} 
+int sumOdd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sumOdd = sumOdd + array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов,стоящих на нечетных позициях равна {sumOdd}");
