b46d7f412d62e534489db8ad6cf82b8f347235aa
Marina Mordovkina <m_mordovkina@mail.ru>
2022-11-16 16:40:22 +0300

    Программа расчета разницы между максимальным и минимальным элементами массива

---
+++ Seminar5_DZ3/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+
+double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + " ");
+} 
+
+int minPosition = 0;
+
+//нахождение минимального
+void MinArray(double[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if(array[i] < array[minPosition])
+            {
+                minPosition = i;
+            }
+    }
+Console.WriteLine("");
+Console.WriteLine($"Минимальный элемент {array[minPosition]}");
+}
+
+//нахождение максимального
+int maxPosition = 0;
+void MaxArray(double[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if(array[i] > array[maxPosition])
+            {
+                maxPosition = i;
+            }
+    }
+ Console.WriteLine($"Максимальный элемент {array[maxPosition]}");
+ Console.WriteLine("");
+ }
+
+MinArray(array);
+MaxArray(array);
+Console.WriteLine($"Разница {array[maxPosition]-array[minPosition]}");
+
+ 
