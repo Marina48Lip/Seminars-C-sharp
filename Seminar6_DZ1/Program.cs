b7f41133ab30c3c6a48917e4cd4a5f50b39d857d
Marina Mordovkina <m_mordovkina@mail.ru>
2022-11-20 20:58:57 +0300

    Программа определения количества введенных пользователем чисел, которые больше 0

---
+++ Seminar6_DZ1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[M];
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine($"Введите число под номером {i + 1}");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    if (array[i] > 0) count = count + 1;
+}
+Console.Write($"Вы ввели {count} числа больше 0");
