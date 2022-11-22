6cea2f0c367b93ead028b21ad7f548c90ab700dd
Marina Mordovkina <m_mordovkina@mail.ru>
2022-11-22 15:59:29 +0300

    Программа вывода на экран двумерного массива, заполенного случ.вещественными числами

---
+++ Seminar7_DZ1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Запросите от пользователя размерности двумерного массива. 
+Напишите метод для заполнения массива случайными числами. 
+Напишите метод для вывода массива на экран (постарайтесь вывести массив красиво). 
+Округлите значения, генерируемые Random до двух знаков после запятой.*/
+
+
+Console.Write("Введите М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            Console.Write(Math.Round(array[i, j], 2) + "  ");
+        }
+    Console.WriteLine();
+}
+}
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            array[i, j] = new Random().NextDouble() * new Random().Next(-10, 10);
+        }
+    }
+}
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
