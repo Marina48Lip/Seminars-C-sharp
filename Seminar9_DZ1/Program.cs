a16a384f4a6540751023dce643a67cabdfd50509
Marina Mordovkina <m_mordovkina@mail.ru>
2022-11-29 11:57:56 +0300

    Программа вывода чисел от N до 1 с помощью рекурсии

---
+++ Seminar9_DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int NumbersRec(int n)
+{
+    if (n == 1) return 1;
+    Console.Write (n + ", ");
+    return NumbersRec(n - 1);
+}
+
+Console.WriteLine(NumbersRec(n)); 
